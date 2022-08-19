2675495ba33a1ebde1ebe0fa32250f76cdea7c5a
Roman Medvedev <romankazan88@gmail.com>
2022-08-19 12:09:49 +0500

    Test1

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data1"))
+                .body("data", equalTo("some data"))
         ;
     }
 
