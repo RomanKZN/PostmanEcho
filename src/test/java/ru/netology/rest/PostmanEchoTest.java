2119ac2c9a3f6772ea670a748cad3196059ae52e
Roman Medvedev <romankazan88@gmail.com>
2022-08-19 12:05:13 +0500

    Readme.md

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -9,8 +9,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
 
     @Test
-
-    void shouldReturnBody(){
+    void shouldReturnBody() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
